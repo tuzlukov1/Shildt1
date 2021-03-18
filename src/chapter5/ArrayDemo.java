08e28471b93aca43b0667da6d5048878bd6456d5
yan.tuzlukov <yan.tuzlukov@darix.com>
2021-03-18 23:58:33 +0700

    nice

---
+++ src/chapter5/ArrayDemo.java
@@ -0,0 +1,14 @@
+package chapter5;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int sample[] = new int[10];
+        int i;
+
+        for(i = 0; i < 10; i = i+1)
+            sample[i] = i;
+
+        for(i = 0; i < 10; i = i+1)
+            System.out.println("Ёлемент[" + i + "]: " + sample[i]);
+    }
+}
