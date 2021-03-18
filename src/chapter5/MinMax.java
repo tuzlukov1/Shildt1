dbf237bb0ef2717e2c8d1965752e85cb83c1bd81
yan.tuzlukov <yan.tuzlukov@darix.com>
2021-03-19 00:04:35 +0700

    nice

--- src/chapter5/MinMax.java
+++ src/chapter5/MinMax.java
@@ -19,6 +19,8 @@ public class MinMax {
         min = max = nums[0];
         for (int i = 1; i < 10; i++) {
             if(nums[i] < min ) min = nums[i];
+            if(nums[i] > max ) max = nums[i];
         }
+        System.out.println("min è max : " + min + " " + max);
     }
 }
