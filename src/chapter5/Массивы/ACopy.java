ee1f861e980ab074e1c1d524b01bcc00f98e1a4a
tuzlukov1 <tyzluckoff@yandex.ru>
2021-03-25 01:09:11 +0700

    array study

---
+++ src/chapter5/Массивы/ACopy.java
@@ -0,0 +1,20 @@
+package chapter5.Массивы;
+//Пример использования переменной length для копирования массивов
+public class ACopy {
+    public static void main(String[] args) {
+        int i;
+        int nums1[] = new int[10];
+        int nums2[] = new int[10];
+
+        for (i = 0; i < nums1.length; i++)
+            nums1[i] = i;
+
+        //копирование массива nums1 в массив nums2
+        if(nums2.length >= nums1.length) //использование перменной length для сравнения размеров массивов
+            for (i = 0; i < nums2.length; i++)
+                nums2[i] = nums1[i];
+
+            for (i = 0; i < nums2.length; i++)
+                System.out.print(nums2[i] + " ");
+    }
+}
