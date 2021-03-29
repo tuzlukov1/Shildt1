04ec5df4dc3f53fc523aaa0ea025ea30bf2e91e7
tuzlukov1 <tyzluckoff@yandex.ru>
2021-03-30 00:16:36 +0700

    array study

---
+++ src/chapter5/Массивы/Search.java
@@ -0,0 +1,21 @@
+package chapter5.ћассивы;
+//ѕоиск в массиве с использованием расширенного цикла for
+
+public class Search {
+    public static void main(String[] args) {
+        int nums[] = {6, 8, 3, 7, 5, 6, 1, 4};
+        int val = 5;
+        boolean found = false;
+
+        //использование цикла типа for-each дл€ поиска значени€ переменной val в массиве nums
+        for (int x : nums) {
+            if (x == val) {
+                found = true;
+                break;
+            }
+        }
+        if(found)
+            System.out.println("«начение найдено!");
+
+    }
+}
