4fc6caacfdc24f22eaa19936b5256a239bd1ef9b
tuzlukov1 <tyzluckoff@yandex.ru>
2021-04-01 00:13:12 +0700

    string study

---
+++ src/chapter5/String/StrOps.java
@@ -0,0 +1,46 @@
+package chapter5.String;
+//операции над строками
+
+public class StrOps {
+    public static void main(String[] args) {
+        String str1 = "Java - лидер интернета!";
+        String str2 = new String(str1);
+        String str3 = "Строки Java эффективны";
+        int result, idx;
+        char ch;
+
+        System.out.println("Длина строки str1: " + str1.length());
+
+        for (int i = 0; i < str1.length(); i++)
+            System.out.print(str1.charAt(i));
+        System.out.println();
+
+        if(str1.equals(str2))
+            System.out.println("str1 эквивалентна str2");
+        else
+            System.out.println("str1 не эквивалентна str2");
+
+        if(str1.equals(str3))
+            System.out.println("str1 эквивалентна str3");
+        else
+            System.out.println("str1 не эквивалентна str3");
+
+        result = str1.compareTo(str3);
+        if(result == 0)
+            System.out.println("str1 и str3 равны");
+        else if(result < 0)
+            System.out.println("str1 меньше str3");
+        else
+            System.out.println("str1 больше str3");
+
+        //присваивание переменной str2 новой строки
+        str2 = "One Two Three One";
+
+        idx = str2.indexOf("One");
+        System.out.println("Индекс первого вхождения One:" + idx);
+        idx = str2.lastIndexOf("One");
+        System.out.println("Индекс последнего вхождения One: " + idx);
+
+    }
+
+}
---
+++ src/chapter5/String/StringSwitch.java
@@ -0,0 +1,27 @@
+package chapter5.String;
+//Использование строк для управления инструкцией switch
+
+public class StringSwitch {
+    public static void main(String[] args) {
+
+        String command = "cancel";
+
+        switch (command) {
+            case "connect":
+                System.out.println("Подключение");
+                break;
+
+            case "cancel":
+                System.out.println("Отмена");
+                break;
+
+            case "disconnect":
+                System.out.println("Отключение");
+                break;
+
+            default:
+                System.out.println("Неверная команда!");
+                break;
+        }
+    }
+}
