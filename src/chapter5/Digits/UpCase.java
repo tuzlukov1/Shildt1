e00f78ed511f54d7949cdb872f0d5b9fe73e6e4f
tuzlukov1 <tyzluckoff@yandex.ru>
2021-04-05 22:46:55 +0700

    String[] args study

---
+++ src/chapter5/Digits/UpCase.java
@@ -0,0 +1,19 @@
+package chapter5.Digits;
+
+public class UpCase {
+    public static void main(String[] args) {
+        char ch;
+
+        for (int i = 0; i < 10; i++) {
+            ch = (char)('a' + i);
+            System.out.print(ch);
+
+            //В следующем операторе сбрасывается шестой бит.
+            //после этого в переменной ch будет храниться код.
+            //Символа прописной группы.
+            ch = (char) ((int)ch & 65503);
+
+            System.out.print(ch + " ");
+        }
+    }
+}
