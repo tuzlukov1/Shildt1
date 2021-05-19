d1714bda41161f25b4baafded9bef29141670567
tuzlukov1 <tyzluckoff@yandex.ru>
2021-05-20 00:54:49 +0700

    Enternum.java

---
+++ src/JavaRush/Enternum.java
@@ -0,0 +1,16 @@
+package JavaRush;
+
+import java.util.Scanner;
+
+public class Enternum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+
+        int number = sc.nextInt();
+
+        System.out.println("Спасибо! Вы ввели число " + number);
+
+    }
+}
